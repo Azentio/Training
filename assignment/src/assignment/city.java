808915ae189386ec23e5614310c23aecf32724eb
arulgurunathan.2973 <arulgurunathan.2973@X34.mnw.local>
2021-10-01 20:38:38 +0530

    Initial commit

---
+++ assignment/src/assignment/city.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class city {
+public static void main(String[] Args) {
+	String[] a = new String[5];
+	a[0] = "Salem";
+	a[1] = "Erode";
+	a[2] = "Namakkal";
+	a[3] = "Karur";
+	a[4] = "Dharmapuri";
+	
+	for(String str :a)
+	{
+		System.out.println(str);
+	}
+}
+
+}
---
+++ assignment/src/assignment/name.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class name {
+	
+	public static void main(String[] Args) {
+		for(int i =0;i<10;i++) {
+			System.out.println("Arul");
+		}
+	}
+
+}
