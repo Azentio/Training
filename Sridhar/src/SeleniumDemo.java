1db81cd8e059e14bfcebe514c62da3bac2ad74a5
Sridhar.2010 <Sridhar.2010@G025.mnw.local>
2021-10-07 16:47:30 +0530

    Initial commit

--- Sridhar/src/SeleniumDemo.java
+++ Sridhar/src/SeleniumDemo.java
@@ -5,8 +5,8 @@ public class SeleniumDemo {
 
 	public static void main(String[] args) throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sridhar.2010\\git\\repository\\Sridhar\\driver\\chromedriver.exe");
-		WebDriver d=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sridhar.2010\\git\\repository\\Sridhar\\driver\\chromedriver.exe");// depending on browser we have to change the path
+		WebDriver d=new ChromeDriver();//depending on browser we have to chage the child class
         d.get("http://omayo.blogspot.com/");
         Thread.sleep(2000);
         d.manage().window().maximize();
