3ccc4a0b9dad4af2e52b825fdc7f21cdf1a4b4a6
tushar.maheshkar <tushar.maheshkar@ANW-WIN-D097.tmnw.net>
2021-10-07 12:52:45 +0530

    Automated Code

--- TusharDesk_Training/src/Demo.java
+++ TusharDesk_Training/src/Demo.java
@@ -1,9 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		//ChromeDriver is a predefined class of selenium WebDriver library
 		//Create an object for ChromeDriver class
@@ -12,8 +13,26 @@ public class Demo {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\tushar.maheshkar\\eclipse-workspace\\TusharDesk_Training\\WebDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://omayo.blogspot.com/");
 		
+		driver.get("http://omayo.blogspot.com/");
+
+		Thread.sleep(4000);
+		
+		
+		
+		
+		driver.manage().window().maximize();
+		Thread.sleep(4000);
+		
+		driver.findElement(By.linkText("Open a popup window")).click();
+		
+		
+		
+		
+		
+		Thread.sleep(7000);
+		
+		driver.quit();
 		
 		
 
