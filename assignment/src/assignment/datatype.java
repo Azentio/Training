83a0f09061a8682625a7f27fb9cf2f42e29b94e6
arulgurunathan.2973 <arulgurunathan.2973@X34.mnw.local>
2021-10-12 15:28:25 +0530

    Initial commit

---
+++ assignment/src/assignment/datatype.java
@@ -0,0 +1,25 @@
+package assignment;
+
+public class datatype {
+
+	
+	public static void main(String[] args) {
+		int a = 10;
+		byte b = 5;
+		short c = 15;
+		long d = 20;
+		float e = (float) 1.5;
+		double f = 6.5;
+		byte g = 100;
+		byte h = 010;
+		
+		
+		
+		// TODO Auto-generated method stub
+		System.out.println(a+b+c+d);
+		System.out.println(e+f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+
+}
